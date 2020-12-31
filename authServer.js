913a7c45726949f48268a6d08da0f04ff2e1ecfa
fadooda <fadyibrahim37@gmail.com>
2020-12-31 04:29:25 -0500

    Refactored and cleaned up code base.

--- authServer.js
+++ authServer.js
@@ -5,29 +5,22 @@ const jwt = require('jsonwebtoken');
 const dbConnect = require('./connectDB');
 const bcrypt = require('bcrypt');
 const app = express()
-//var bodyParser = require('body-parser');
-//var urlencodedParser = bodyParser.urlencoded({ extended: false});
+
 
 app.options('/login', cors()) // enable pre-flight request for DELETE request
 app.options('/register', cors()) // enable pre-flight request for DELETE request
 app.use(express.json())
-//var cors = require('cors')
 
-//app.use(cors()) // Use this after the variable declaration
 
 var refreshTokenList = []
 
 
 app.post('/login',cors(), (req,res)=>{
-    //const username = req.body.username
-    //const password = req.body.password
-    //console.log('name '+ password)
-    //db.collection.find_one({'ip': '61.228.93.0'})['history']
+
     console.log(process.env.REFRESH_TOKEN_SECRET)
     let user= {"userName": req.body.userName}
     dbConnect.getUser(user, async function(dbuser /* a is passed using callback */) {
-        //console.log(a); // a is the object return by the database
-        //console.log(dbuser)
+
         if(dbuser==null)
         {
             console.log("User doesn't Exist")
@@ -44,26 +37,18 @@ app.post('/login',cors(), (req,res)=>{
                 str+= " Incorrect password"
                 res.sendStatus(401)
             }
-
-            //res.json({str})
         }
     })
-    //const user = { name: username}
-    //const accessToken = generateAccessToken(user)
-    //const refreshToken = jwt.sign(user,process.env.REFRESH_TOKEN_SECRET)
-    //refreshTokenList.push(refreshToken) //push it to the database
-    //res.json({accessToken: accessToken, refreshToken: refreshToken})
+
     })
 
 app.post('/register',cors(), (req,res)=>{
-    //dbConnect.addUser(req)
-    //console.log(req.body.userName)
-    //var dbuser;
+
     let user= {"userName": req.body.userName}
+
     //use call back to prevent the server from executing the next line of codes until the database returns 
     dbConnect.getUser(user, async function(dbuser /* a is passed using callback */) {
-        //console.log(dbuser); // a is the object return by the database
-       //console.log(dbuser.userName)
+
         if(dbuser==null)
         {
             console.log(":::Registering User:::")
@@ -74,10 +59,29 @@ app.post('/register',cors(), (req,res)=>{
             let str="user: " + dbuser.userName +" already exists"
             console.log(str)
             res.sendStatus(409)
-            //res.json({str})
         }
     })
     })
+
+app.get('/games/authenticate',cors(), authenticateToken, (req,res)=>{
+    res.json(true)
+})
+        
+function authenticateToken(req,res,next)
+{
+     const authHeader = req.headers['authorization']
+     const token = authHeader && authHeader.split(' ')[1]
+     if(token == null) 
+     {
+         return res.sendStatus(401)
+     }
+
+     jwt.verify(token,process.env.ACCESS_TOKEN_SECRET,(err,user)=>{
+         if(err) return res.sendStatus(401)
+         req.user = user 
+         next()
+     })
+}
 
 function generateAccessToken(user)
 {
